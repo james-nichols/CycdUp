e84bf435a845d8c61f7f6d3fe6310e69a7c63581
James-Nichols <james.ashton.nichols@gmail.com>
2015-09-25 13:30:41 +0200

    slowed rate to about 60 per second

--- CycdUpArdiuno/CycdUpArdiuno.ino
+++ CycdUpArdiuno/CycdUpArdiuno.ino
@@ -6,23 +6,25 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
+ 
 }
 
 void loop() {
-  
+
   analogVals[0] = analogRead(A0);
   analogVals[1] = analogRead(A1);
   analogVals[2] = analogRead(A2);
   analogVals[3] = analogRead(A3);
-  
-  for (int i=0; i<4; i++) {
-    
+
+  for (int i = 0; i < 4; i++) {
+
     Serial.write(0xff);
     Serial.write(0xff);
     Serial.write(i);
     Serial.write(lowByte(analogVals[i]));
     Serial.write(highByte(analogVals[i]));
-    
+
   }
-  delay(10);
+  // THIS SHOULD BE CHANGED TO MAKE AN APPROPRIATE RATE!
+  delay(16);
 }
